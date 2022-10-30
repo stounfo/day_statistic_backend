2127aab3168f463f061460a142da86477b8b603f
Levantin Oleg <stounfo@gmail.com>
2022-10-30 22:33:54 +0300

    Small AccessCodeStr refactoring

--- app/common/types/types.py
+++ app/common/types/types.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
 
 import re
+import secrets
 from typing import Dict, TypeAlias
 
 from pydantic.validators import str_validator
 
 from app.common.types.errors import AccessCodeError
-from app.config import settings
 
 UsernameStr: TypeAlias = str
 
 
 class AccessCodeStr(str):
+    length = 6
+
     @classmethod
     def __get_validators__(cls):
         yield str_validator
@@ -21,7 +23,7 @@ class AccessCodeStr(str):
     def validate(cls, value: AccessCodeStr) -> AccessCodeStr:
         if any(
             [
-                len(value) != settings.app.user_settings.access_code_len,
+                len(value) != cls.length,
                 not re.compile(r"[0-9]").match(value),  # only digits
             ],
         ):
@@ -31,15 +33,17 @@ class AccessCodeStr(str):
     @classmethod
     def __modify_schema__(cls, field_schema: Dict[str, str]):
         field_schema.update(
-            examples="".join(
-                (
-                    str(i)
-                    for i in range(settings.app.user_settings.access_code_len)
-                )
-            ),
+            examples="".join((str(i) for i in range(cls.length))),
             description=(
-                "Must only contain digits and must be"
-                f" {settings.app.user_settings.access_code_len} characters"
+                f"Must only contain digits and must be {cls.length} characters"
                 " long"
             ),
+        )
+
+    @classmethod
+    def random(cls) -> AccessCodeStr:
+        return cls(
+            "".join(
+                [str(secrets.choice(range(10))) for i in range(cls.length)]
+            )
         )
